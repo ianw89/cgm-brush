b55b73efe271a0811a57fa407c0329931dc13208
Ian Williams <ianw89@live.com>
2021-07-23 17:30:15 -0700

    Chapters 2 and 3 merged into module

--- src/cgmbrush.py
+++ src/cgmbrush.py
@@ -1,4 +1,3 @@
-# imports packages
 from __future__ import print_function 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -7,13 +6,12 @@ from numpy import genfromtxt
 from random import randrange
 from numpy.polynomial import Polynomial as P
 from IPython.display import Latex
+import pandas as pd
+import os
 
 ########################################
 # Paramters, Constants, and Functions
 ########################################
-
-#cosmological parameters and constants
-
 
 # Constants
 Mpc = 3.096*10**24 # cm
@@ -257,3 +255,73 @@ def RS_array_gen(z_max,L):
     return RS_array
 
     
+
+########################################
+# This section extracts the density field of the 256 grid from the simulation tables
+########################################
+
+
+# Function to import density and halo tables for a given redshift
+# TODO I think this should be made to work with 512 resolution too
+def import_density_field_256(redshift):
+    
+    # Have to hardcode z=0 table because of the unique column names
+    if redshift == 0:
+        # reading density field and halos data
+        file_path= os.path.join(sims_folder, 'dens256-z-0.csv.gz')
+        pdDens=pd.read_csv(file_path)
+
+        # extracting columns
+        pdDensN=pdDens[['Bolshoi__Dens256_z0__ix','Bolshoi__Dens256_z0__iy','Bolshoi__Dens256_z0__iz','Bolshoi__Dens256_z0__dens']]
+
+        # 3D density array
+        pdDensN=pdDensN.sort_values(['Bolshoi__Dens256_z0__ix','Bolshoi__Dens256_z0__iy','Bolshoi__Dens256_z0__iz'])
+        tden = pdDensN['Bolshoi__Dens256_z0__dens'].values
+        tden2=np.reshape(tden,(256,256,256))
+
+        return ((tden2+1).sum(2))*10**6* dx* elecD(z) /(1+z)**2
+
+    else:
+        name = 'dens256-z-0.'+str(redshift)+'.csv.gz'
+        den = pd.read_csv(name)
+        den2=den[['Bolshoi__Dens256__ix','Bolshoi__Dens256__iy','Bolshoi__Dens256__iz','Bolshoi__Dens256__dens']]
+
+        # 3D density array
+        den_sorted=den2.sort_values(['Bolshoi__Dens256__ix','Bolshoi__Dens256__iy','Bolshoi__Dens256__iz'])
+        den_vals = den_sorted['Bolshoi__Dens256__dens'].values
+        den256=np.reshape(den_vals,(256,256,256))
+        
+        return normDM((den256+1).sum(2),0)
+    #     return normDM((den256+1).sum(random.randint(0,1)),0)
+
+
+# Create a single array of density fields for various redshifts
+# Density fields: Choose 256 or 512 field
+def extract_all_den_fields(RS_array,den_grid_size):
+
+
+    all_den_fields = np.zeros([len(RS_array),den_grid_size,den_grid_size])
+
+    if den_grid_size == 256:
+        all_den_fields[0,:,:] = import_density_field_256(0)
+    elif den_grid_size == 512:
+        all_den_fields[0,:,:] = import_density_field_512(0) # BUG This is undefined... generalize the above?
+
+    for i in range(1, len(RS_array)):
+        if den_grid_size == 256:
+            all_den_fields[i,:,:]=import_density_field_256(i)
+        elif den_grid_size == 512:
+            all_den_fields[i,:,:]=import_density_field_512(i)
+    
+    return all_den_fields 
+    
+    
+    
+    
+# Extract halos for a given redshift
+def extract_halos(redshift):
+
+    name = 'halo-z-0.'+str(redshift)+'.csv.gz'    
+    file_path= os.path.join(sims_folder, name)
+    halos = pd.read_csv(file_path)
+    return halos
