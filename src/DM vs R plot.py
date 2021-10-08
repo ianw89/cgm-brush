63d688799089f2707284372027612400dc2df5f9
Ian Williams <ianw89@live.com>
2021-10-08 16:12:21 -0700

    Improve DM vs R plot

--- src/DM vs R plot.py
+++ src/DM vs R plot.py
@@ -3,19 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.patches import Rectangle
 
-# Resolution: choose between 256 and 512 grid
-den_grid_size = 256
-# User provides a redshift array
-RS_array = [0] # For a single box, we only use the redshift 0 box
-# Profile used for subtracting halos from the density field
-subtraction_halo_profile = 'NFW'
-# Mass range of halos
-min_mass=10**10
-max_mass=10**14.5
-log_bins=30
-
-M_chosen = [1,10,15,20,25]
-
+M_chosen = [1,5,12,18,25] # only the middle 3 are used
 provider = BolshoiProvider()
 
 # Specify resolution
@@ -30,24 +18,31 @@ load_masks = True
 
 series = []
 
-config = Configuration(SphericalTophatProfile(), 1, provider=provider, resolution=resolution, folder=varFolder)
+config = Configuration(SphericalTophatProfile(), 1, provider=provider, resolution=1, folder=varFolder)
 config.datestamp = '2021-10-05'
 config.run(load_from_files=load_data)
-config.generate_DM_vs_radius_profile(load_from_files=load_DM_vs_rad)
-config.generate_profile_of_masks(load_from_files=load_masks)
-STH_DMvsR = config.DM_vs_R1
-STH_masks = config.mask_profiles
-
 # Table of virial radii and avg masses
 vir_rad_ar = config.results['vir_radii']
 avg_mass_ar = config.results['halo_masses']
 
+print ("Masses chosen: ")
+print (avg_mass_ar[M_chosen[1]])
+print (avg_mass_ar[M_chosen[2]])
+print (avg_mass_ar[M_chosen[3]])
+
+config = Configuration(SphericalTophatProfile(), 1, provider=provider, resolution=resolution, folder=varFolder)
+config.datestamp = '2021-10-05'
+#config.run(load_from_files=load_data)
+config.generate_DM_vs_radius_profile(load_from_files=load_DM_vs_rad)
+config.generate_profile_of_masks(load_from_files=load_masks)
+STH_DMvsR = config.DM_vs_R1
+STH_masks = config.mask_profiles
 config.clear_results()
 series.append((config.DM_vs_R1,config.mask_profiles,'3D Tophat','red'))
 
 config = Configuration(SphericalTophatProfile(), 2, provider=provider, resolution=resolution, folder=folder)
 config.datestamp = '2021-10-05'
-config.run(load_from_files=load_data)
+#config.run(load_from_files=load_data)
 config.generate_DM_vs_radius_profile(load_from_files=load_DM_vs_rad)
 config.generate_profile_of_masks(load_from_files=load_masks)
 STH_2_DMvsR = config.DM_vs_R1
@@ -57,7 +52,7 @@ series.append((config.DM_vs_R1, config.mask_profiles, '3D Tophat 2$R_{vir}$', 'o
 
 config = Configuration(FireProfile(), 1, provider=provider, resolution=resolution, folder=folder)
 config.datestamp = '2021-10-05'
-config.run(load_from_files=load_data)
+#config.run(load_from_files=load_data)
 config.generate_DM_vs_radius_profile(load_from_files=load_DM_vs_rad)
 config.generate_profile_of_masks(load_from_files=load_masks)
 fire_DMvsR = config.DM_vs_R1
@@ -67,7 +62,7 @@ series.append((config.DM_vs_R1, config.mask_profiles, 'FIRE', 'green'))
 
 config = Configuration(NFWProfile(), 1, provider=provider, resolution=resolution, folder=folder)
 config.datestamp = '2021-10-05'
-config.run(load_from_files=load_data)
+#config.run(load_from_files=load_data)
 config.generate_DM_vs_radius_profile(load_from_files=load_DM_vs_rad)
 config.generate_profile_of_masks(load_from_files=load_masks)
 NFW_DMvsR = config.DM_vs_R1
@@ -77,7 +72,7 @@ series.append((config.DM_vs_R1, config.mask_profiles, 'NFW', 'blue'))
 
 config = Configuration(PrecipitationProfile(), 1, provider=provider, resolution=resolution, folder=folder)
 config.datestamp = '2021-10-05'
-config.run(load_from_files=load_data)
+#config.run(load_from_files=load_data)
 config.generate_DM_vs_radius_profile(load_from_files=load_DM_vs_rad)
 config.generate_profile_of_masks(load_from_files=load_masks)
 P_DMvsR = config.DM_vs_R1
@@ -85,47 +80,13 @@ P_masks = config.mask_profiles
 config.clear_results()
 series.append((config.DM_vs_R1, config.mask_profiles, 'Precipitation', 'c'))
 
-
-
-### For error bar plot
-# Variances should be extracted from the redshift plots at redshift =0.5
-var_STH_2 = 173
-var_STH2_2 = 141
-var_NFW_2 = 451
-var_fire_2 = 226
-var_P_2 = 215
-
-sd_incl_host = np.sqrt(var_fire_2**2 + 300**2)
-
-
 def error_bar_DMvsRad(sd,N_frb,radius_array,Rvir,avg_frbs):
+    """avg_frbs is how many halos of this size an FRB goes through; see McQuinn 2014."""
     error_bar = np.zeros([len(radius_array)])
     for i in range(1,len(radius_array)):
         error_bar[i-1] = sd/(np.sqrt(avg_frbs*N_frb*((radius_array[i]**2-radius_array[i-1]**2)/Rvir**2)))
     
     return error_bar
-
-
-
-#r_star_ar_1 = np.logspace(np.log10(vir_rad_ar[M_chosen[1]]/25),np.log10(3*vir_rad_ar[M_chosen[1]]),10)
-#r_star_ar_2 = np.logspace(np.log10(vir_rad_ar[M_chosen[2]]/25),np.log10(3*vir_rad_ar[M_chosen[2]]),10)
-r_star_ar = np.logspace(np.log10(vir_rad_ar[M_chosen[3]]/25),np.log10(3*vir_rad_ar[M_chosen[3]]),10)
-
-error_100_11M_STH = error_bar_DMvsRad(var_STH_2,100,r_star_ar,vir_rad_ar[M_chosen[1]],4)
-error_100_11M_fire = error_bar_DMvsRad(var_fire_2,100,r_star_ar,vir_rad_ar[M_chosen[1]],4)
-error_100_11M_P = error_bar_DMvsRad(var_P_2,100,r_star_ar,vir_rad_ar[M_chosen[1]],4)
-
-error_100_12M_STH = error_bar_DMvsRad(var_STH_2,100,r_star_ar,vir_rad_ar[M_chosen[2]],2)
-error_100_12M_fire = error_bar_DMvsRad(var_fire_2,100,r_star_ar,vir_rad_ar[M_chosen[2]],2)
-error_100_12M_P = error_bar_DMvsRad(var_P_2,100,r_star_ar,vir_rad_ar[M_chosen[2]],2)
-
-error_100_13M_STH = error_bar_DMvsRad(var_STH_2,100,r_star_ar,vir_rad_ar[M_chosen[3]],.2)
-error_100_13M_fire = error_bar_DMvsRad(var_fire_2,100,r_star_ar,vir_rad_ar[M_chosen[3]],.2)
-error_100_13M_P = error_bar_DMvsRad(var_P_2,100,r_star_ar,vir_rad_ar[M_chosen[3]],.2)
-
-# error when host halo DM is included
-error_100_12M_fire_host = error_bar_DMvsRad(sd_incl_host,100,r_star_ar,vir_rad_ar[M_chosen[2]],2)
-
 
 def plot_DM_vs_Rad(x_axis, mean_DM, axis, massbin, series, plot_masks):
     
@@ -142,7 +103,7 @@ def plot_error_bars(r_star_ar, axis, *lines):
         axis.semilogx(r_star_ar*1000,line[0],ls='--',drawstyle='steps',color=line[1],lw=line[2])
 
 
-def make_DM_vs_Rad_profiles_plots(series, error: bool):
+def make_DM_vs_Rad_profiles_plots(series, error: bool, x_start, x_end):
 
     orig_den_256 = provider.get_density_field(0, 256)
 
@@ -169,7 +130,6 @@ def make_DM_vs_Rad_profiles_plots(series, error: bool):
     plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
 
     MpctoKpc =1000
-    x_start = 30  #kpc: min impact parameter 
     hspace = 0.015
 
     DM_Rad_fig, DM_Rad_axs = plt.subplots(3, 1,
@@ -184,6 +144,38 @@ def make_DM_vs_Rad_profiles_plots(series, error: bool):
     plot_DM_vs_Rad(x_axis, mean_DM, DM_Rad_axs[2], M_chosen[3], series, not error)
 
     if error:
+
+        ### For error bar plot
+        # Variances should be extracted from the redshift plots at redshift =0.5
+        var_STH_2 = 173
+        var_STH2_2 = 141
+        var_NFW_2 = 451
+        var_fire_2 = 226
+        var_P_2 = 215
+
+        sd_incl_host = np.sqrt(var_fire_2**2 + 300**2)
+
+        #r_star_ar_1 = np.logspace(np.log10(vir_rad_ar[M_chosen[1]]/25),np.log10(3*vir_rad_ar[M_chosen[1]]),10)
+        #r_star_ar_2 = np.logspace(np.log10(vir_rad_ar[M_chosen[2]]/25),np.log10(3*vir_rad_ar[M_chosen[2]]),10)
+        r_star_ar = np.logspace(np.log10(vir_rad_ar[M_chosen[3]]/30),np.log10(4*vir_rad_ar[M_chosen[3]]),10)
+
+        num_FRB = 100
+
+        error_100_11M_STH = error_bar_DMvsRad(var_STH_2,num_FRB,r_star_ar,vir_rad_ar[M_chosen[1]],4)
+        error_100_11M_fire = error_bar_DMvsRad(var_fire_2,num_FRB,r_star_ar,vir_rad_ar[M_chosen[1]],4)
+        #error_100_11M_P = error_bar_DMvsRad(var_P_2,num_FRB,r_star_ar,vir_rad_ar[M_chosen[1]],4)
+
+        error_100_12M_STH = error_bar_DMvsRad(var_STH_2,num_FRB,r_star_ar,vir_rad_ar[M_chosen[2]],2)
+        error_100_12M_fire = error_bar_DMvsRad(var_fire_2,num_FRB,r_star_ar,vir_rad_ar[M_chosen[2]],2)
+        #error_100_12M_P = error_bar_DMvsRad(var_P_2,num_FRB,r_star_ar,vir_rad_ar[M_chosen[2]],2)
+
+        error_100_13M_STH = error_bar_DMvsRad(var_STH_2,num_FRB,r_star_ar,vir_rad_ar[M_chosen[3]],0.2)
+        error_100_13M_fire = error_bar_DMvsRad(var_fire_2,num_FRB,r_star_ar,vir_rad_ar[M_chosen[3]],0.2)
+        #error_100_13M_P = error_bar_DMvsRad(var_P_2,num_FRB,r_star_ar,vir_rad_ar[M_chosen[3]],0.2)
+
+        # error when host halo DM is included
+        error_100_12M_fire_host = error_bar_DMvsRad(sd_incl_host,100,r_star_ar,vir_rad_ar[M_chosen[2]],2)
+
         plot_error_bars(r_star_ar, DM_Rad_axs[0], (error_100_11M_STH, 'red', 4), (error_100_11M_fire, 'green', 4))
         plot_error_bars(r_star_ar, DM_Rad_axs[1], (error_100_12M_STH, 'red', 4), (error_100_12M_fire, 'green', 4), (error_100_12M_fire_host, 'blue', 2))
         plot_error_bars(r_star_ar, DM_Rad_axs[2], (error_100_13M_STH, 'red', 4), (error_100_13M_fire, 'green', 4))
@@ -220,22 +212,25 @@ def make_DM_vs_Rad_profiles_plots(series, error: bool):
     DM_Rad_axs[2].axvline(MpctoKpc*(vir_rad_ar[M_chosen[3]]), color='k', linestyle='--', linewidth=1)
 
     # x and y axis limits
-    DM_Rad_axs[0].set_ylim([0, 130])
-    DM_Rad_axs[0].set_xlim([x_start, .7*MpctoKpc])
-    DM_Rad_axs[1].set_ylim(ymin=0,ymax=399)
-    DM_Rad_axs[1].set_xlim([x_start, .7*MpctoKpc])
-    DM_Rad_axs[2].set_ylim(ymin=0,ymax=1499)
-    DM_Rad_axs[2].set_xlim([x_start, .7*MpctoKpc])
+    ymax1 = 129
+    ymax2 = 309
+    ymax3 = 799
+    DM_Rad_axs[0].set_ylim([0, ymax1])
+    DM_Rad_axs[0].set_xlim([x_start, x_end])
+    DM_Rad_axs[1].set_ylim(ymin=0,ymax=ymax2)
+    DM_Rad_axs[1].set_xlim([x_start, x_end])
+    DM_Rad_axs[2].set_ylim(ymin=0,ymax=ymax3)
+    DM_Rad_axs[2].set_xlim([x_start, x_end])
 
-    # Rectangular patch 
-    DM_Rad_axs[0].add_patch(Rectangle((0,0), 45, 130,facecolor='yellow'))
-    DM_Rad_axs[1].add_patch(Rectangle((0,0), 45, 399,facecolor='yellow'))
-    DM_Rad_axs[2].add_patch(Rectangle((0,0), 45, 1500,facecolor='yellow'))
+    # Rectangular patch for region too far inside resolution limit
+    #DM_Rad_axs[0].add_patch(Rectangle((0,0), 45, 130,facecolor='yellow'))
+    #DM_Rad_axs[1].add_patch(Rectangle((0,0), 45, 399,facecolor='yellow'))
+    #DM_Rad_axs[2].add_patch(Rectangle((0,0), 45, 1500,facecolor='yellow'))
 
     # mass labels
-    DM_Rad_axs[0].text(310, 120, r'$6 \times 10^{11} M_\odot$ ',fontsize=34) #'Mass = %.1E $M_\odot$' % Decimal(df[2][M_chosen[1]]),fontsize=30)
-    DM_Rad_axs[1].text(310, 360,r'$3 \times 10^{12} M_\odot$ ',fontsize=34) #'Mass = %.1E $M_\odot$' % Decimal(df[2][M_chosen[2]]),fontsize=30)
-    DM_Rad_axs[2].text(310, 1350,r'$2 \times 10^{13} M_\odot$ ',fontsize=34) #'Mass = %.1E $M_\odot$' % Decimal(df[2][M_chosen[3]]),fontsize=30)
+    DM_Rad_axs[0].text(x_end*0.5, ymax1*0.85, r'$10^{11} M_\odot$ ',fontsize=34) #'Mass = %.1E $M_\odot$' % Decimal(df[2][M_chosen[1]]),fontsize=30)
+    DM_Rad_axs[1].text(x_end*0.5, ymax2*0.85, r'$10^{12} M_\odot$ ',fontsize=34) #'Mass = %.1E $M_\odot$' % Decimal(df[2][M_chosen[2]]),fontsize=30)
+    DM_Rad_axs[2].text(x_end*0.5, ymax3*0.85, r'$10^{13} M_\odot$ ',fontsize=34) #'Mass = %.1E $M_\odot$' % Decimal(df[2][M_chosen[3]]),fontsize=30)
 
     # DM_Rad_axs[0].text(2*MpctoKpc*.1, 65, 'Mass = %.1E $M_\odot$' % Decimal(df[2][M_chosen[1]]),fontsize=30)
     # DM_Rad_axs[1].text(2*MpctoKpc*.1, 180, 'Mass = %.1E $M_\odot$' % Decimal(df[2][M_chosen[2]]),fontsize=30)
@@ -247,7 +242,7 @@ def make_DM_vs_Rad_profiles_plots(series, error: bool):
     errstr = ''
     if error:
         errstr = '_error'
-    name = 'DMvsRad_profiles_{}{}.pdf'.format(resolution, errstr)
+    name = 'DMvsRad_profiles_{}_{}{}.pdf'.format(resolution, x_end, errstr)
 
     saveFig(name, DM_Rad_fig, bbox_inches='tight')
 
@@ -255,4 +250,11 @@ def make_DM_vs_Rad_profiles_plots(series, error: bool):
 
 
 
-make_DM_vs_Rad_profiles_plots(series, False)
+make_DM_vs_Rad_profiles_plots(series, False, 15, 999)
+make_DM_vs_Rad_profiles_plots([series[0], series[2]], True, 15, 999)
+
+#make_DM_vs_Rad_profiles_plots(series, False, 15, 899)
+#make_DM_vs_Rad_profiles_plots([series[0], series[2]], True, 15, 899)
+
+#make_DM_vs_Rad_profiles_plots(series, False, 15, 799)
+#make_DM_vs_Rad_profiles_plots([series[0], series[2]], True, 15, 799)
